06fe9e03c088d0bf53eb1eedd844f09eb5b5b164
akeefmamoon <akeefmamoon@gmail.com>
2020-09-15 16:03:56 +1000

    added argstats

--- labs/lab01/arg_stats.c
+++ labs/lab01/arg_stats.c
@@ -3,5 +3,21 @@
 
 int main(int argc, char **argv) {
 	(void) argc, (void) argv;
+	if(argc <= 1){
+		printf("Usage: ./arg_stats NUMBER [NUMBER]");
+	}else{
+		int min = 100;
+		int max = 0;
+		int sum = 0;
+		int prod = 1;
+		int mean = 0;
+		for(int i = 1; i < argc; i++){
+			sum += (int)argv[i];
+			if(argv[i] < min) min = argv[i];
+			if(argv[i] > max) max = argv[i];
+			mean = sum/(argc-1);
+			prod *= (int) argv[i];
+		}
+	}
 	return 0;
 }
