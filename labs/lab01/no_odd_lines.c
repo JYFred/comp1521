3821ef10fd0f8e85d0cb1f00c7231ff878781a8e
akeefmamoon <akeefmamoon@gmail.com>
2020-09-15 15:39:41 +1000

    fixed error

--- labs/lab01/no_odd_lines.c
+++ labs/lab01/no_odd_lines.c
@@ -4,12 +4,12 @@
 
 int main(void) {
 	char input[SIZE] = { 0 };
-	fgets(input, SIZE, stdin);
-	//input[strcspn(input, "\n")] = 0;
-	int length = strlen(input);
-	if(length%2==0){
-		fputs(input, stdout);
+	while((fgets(input, SIZE, stdin)!=EOF)){
+		//input[strcspn(input, "\n")] = 0;
+		int length = strlen(input);
+		if(length%2==0){
+			fputs(input, stdout);
+		}
 	}
-
 	return 0;
 }
