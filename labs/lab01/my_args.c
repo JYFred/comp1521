5f666f54962115799dc37d2f7487867e2dbef5ec
akeefmamoon <akeefmamoon@gmail.com>
2020-09-15 15:47:29 +1000

    added myargs

--- labs/lab01/my_args.c
+++ labs/lab01/my_args.c
@@ -2,5 +2,11 @@
 
 int main(int argc, char **argv) {
 	(void) argc, (void) argv;
+	if(argc > 0) printf("Program name: %s", argv[0]);
+	if(argc > 1){
+		for(int i =1; i < argc;i++){
+			printf("Argument %d is %s", i, argv[i]);
+		}
+	}
 	return 0;
 }
