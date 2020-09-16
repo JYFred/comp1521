5063c57d67f94a65b724b9cf615ac3a2af796869
akeefmamoon <akeefmamoon@gmail.com>
2020-09-16 12:37:14 +1000

    added fibonacci

--- labs/lab01/fibonacci.c
+++ labs/lab01/fibonacci.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int memo[47] = {0};
+unsigned int memo[47] = {[0 ... 46] = -1};
+static size_t count = 0;
+
+int fibonacci(int *cache, int n);
 
 int main(void)
 {
+
+	memo[0] = 0;
+	memo[1] = 1;
+
+	int n =0;
+	while(scanf("%d",&n)==1){
+		fibonacci(memo, n);
+	}
 	return EXIT_SUCCESS;
+}
+
+int fibonacci(int *cache, int n){
+
+	count++;
+
+	if(memo[n] == -1){
+		memo[n] = fibonacci(memo, n-1) + fibonacci(memo, n-2); 
+		printf("%d",memo[n]);
+	}
+
+	return cache[n];
+
 }
